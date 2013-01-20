82e26701a195efcdd49ad8b909accd491d7f9bf3
herze <herzed@gmail.com>
2013-01-20 17:46:10 +0100

    Estructura ejercicio Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proyrails::Application.config.secret_token = 'e1c49eef96ce9b0c4a5dec0bfa10c7a5daf9b9373462eeacfa9fb81355b850dae66025d18ed0ebcc7780015b87f4067590d843d49028b4a2564ddb54030787f0'
